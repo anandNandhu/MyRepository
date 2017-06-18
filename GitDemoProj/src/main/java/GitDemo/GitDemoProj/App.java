559049e3d26b941f094b53a8a3c0761673f79408
anandNandhu <anand.nandhu@gmail.com>
2017-06-18 11:10:25 +0530

    Add files via upload

---
+++ GitDemoProj/src/main/java/GitDemo/GitDemoProj/App.java
@@ -0,0 +1,13 @@
+package GitDemo.GitDemoProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!gan+gan" );
+    }
+}
